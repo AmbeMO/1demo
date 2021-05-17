472d7466a43435a8ac1fc229a3b7c60ea4e302bf
AmbeMO <milamo@mail.ua>
2021-05-17 12:15:45 +0300

    alm end html

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,18 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {CatalogComponent} from './catalog/catalog.component';
-import { FormComponent } from './form/form.component'
+import { SignUpComponent } from './signUp/signUp.component';
+import {SignInComponent} from "./signIn/signIn.component";
 import {HeaderComponent} from "./header/header.component";
 import {IndexComponent} from "./index/index.module";
+import {FooterComponent} from "./footer/footer.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CatalogComponent,
-    FormComponent,
+    SignUpComponent,
+    SignInComponent,
     HeaderComponent,
-    IndexComponent
+    IndexComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
